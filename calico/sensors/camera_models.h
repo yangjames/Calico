cb26c0ea772e847fda4d0415a3a2f1b9b9594796
James Yang <james.yang92@gmail.com>
2023-02-24 09:01:21 +0000

    Fixed forward projection for the OpenCv5 model and implemented undistortion.

--- calico/sensors/camera_models.h
+++ calico/sensors/camera_models.h
@@ -33,13 +33,14 @@ class CameraModel {
       const Eigen::VectorX<T>& intrinsics,
       const Eigen::Vector3<T>& point) const;
 
-  // Unproject a pixel and return the corresponding unit ray pointing from
-  // the camera origin to the pixel location. Multiplying this unit vector
-  // by the point's distance gives you the location of that point relative
-  // to the camera resolved in the camera frame.
-  virtual absl::StatusOr<Eigen::Vector3d> UnprojectPixel(
-      const Eigen::VectorXd& intrinsics,
-      const Eigen::Vector2d& pixel) const = 0;
+  // Unproject a pixel and return the corresponding metric plane point. In order
+  // to get the unprojected point in pixel space, apply the pinhole paraeters to
+  // the unprojectd results.
+  // Top level call invokes the derived calss's implementation.
+  template <typename T>
+  absl::StatusOr<Eigen::Vector3<T>> UnprojectPixel(
+      const Eigen::VectorX<T>& intrinsics,
+      const Eigen::Vector2<T>& pixel) const;
 
   // Getter for camera model type.
   virtual CameraIntrinsicsModel GetType() const  = 0;
@@ -48,8 +49,8 @@ class CameraModel {
   virtual int NumberOfParameters() const = 0;
 
   // TODO(yangjames): This method should return an absl::StatusOr. Figure out
-  //                  to support this using unique_ptr's, or find macros that
-  //                  already implement this feature (i.e. ASSIGN_OR_RETURN).
+  //   how to support this using unique_ptr's, or find macros that already
+  //   implement this feature (i.e. ASSIGN_OR_RETURN).
   // Factory method for creating a camera model with `camera_model` type.
   // This method will return a nullptr if an unsupported CameraIntrinsicsModel
   // is passed in.
@@ -86,9 +87,6 @@ class OpenCv5Model : public CameraModel {
           kNumberOfParameters, ". Got ", intrinsics.size()));
     }
     // Prepare values.
-    const T x = point.x() / point.z();
-    const T y = point.y() / point.z();
-    const T r2 = point.squaredNorm();
     const T& f = intrinsics(0);
     const T& cx = intrinsics(1);
     const T& cy = intrinsics(2);
@@ -97,8 +95,11 @@ class OpenCv5Model : public CameraModel {
     const T& p1 = intrinsics(5);
     const T& p2 = intrinsics(6);
     const T& k3 = intrinsics(7);
+    Eigen::Vector2<T> projection(point.x() / point.z(), point.y() / point.z());
+    const T x = projection.x();
+    const T y = projection.y();
+    const T r2 = projection.squaredNorm();
     const T s = T(1.0) + r2 * (k1 + r2 * (k2 + r2 * k3));
-    Eigen::Vector2<T> projection(x, y);
     // Apply radial distortion.
     projection *= s;
     // Apply tangential distortion.
@@ -111,12 +112,62 @@ class OpenCv5Model : public CameraModel {
     return projection;
   }
 
-  absl::StatusOr<Eigen::Vector3d> UnprojectPixel(
-      const Eigen::VectorXd& intrinsics,
-      const Eigen::Vector2d& pixel) const final {
-    return absl::UnimplementedError(absl::StrCat(
-            "UnprojectPixel not yet implemented for camera model ",
-            GetType(), "."));
+  template <typename T>
+  static absl::StatusOr<Eigen::Vector3<T>> UnprojectPixel(
+      const Eigen::VectorX<T>& intrinsics,
+      const Eigen::Vector2<T>& pixel,
+      int max_iterations = 30) {
+    constexpr T kSmallValue = T(1e-14);
+    if (intrinsics.size() != kNumberOfParameters) {
+      return absl::InvalidArgumentError(absl::StrCat(
+          "Expected ", kNumberOfParameters, " parameters for intrinsics. Got ",
+          intrinsics.size()));
+    }
+    const T& f = intrinsics(0);
+    const T& cx = intrinsics(1);
+    const T& cy = intrinsics(2);
+    const T& k1 = intrinsics(3);
+    const T& k2 = intrinsics(4);
+    const T& p1 = intrinsics(5);
+    const T& p2 = intrinsics(6);
+    const T& k3 = intrinsics(7);
+    const T inv_f = T(1.0) / f;
+    // Invert pinhole model from distorted point.
+    const T xd0 = inv_f * (pixel.x() - cx);
+    const T yd0 = inv_f * (pixel.y() - cy);
+    // Set distorted pixel as initial guess.
+    T x = xd0;
+    T y = yd0;
+    // Run Newton's method on the residual of distorted pixel.
+    for (int i = 0; i < max_iterations; ++i) {
+      const T x2 = x * x;
+      const T y2 = y * y;
+      const T r2 = x2 + y2;
+      const T s = T(1.0) + r2 * (k1 + r2 * (k2 + r2 * k3));
+      // Compute residual.
+      const T err_x = xd0 - (s * x + 2 * p1 * x * y + p2 * (r2 + 2 * x2));
+      const T err_y = yd0 - (s * y + 2 * p2 * x * y + p1 * (r2 + 2 * y2));
+      // If residual is small enough, exit early.
+      if (std::abs(x2) + std::abs(y2) < kSmallValue) {
+        break;
+      }
+      // Compute Jacobian:
+      // J = [a, b]
+      //     [b, c]
+      const T ds = 2 * (k1 + r2 * (2 * k2 + 3 * k3 * r2));
+      const T a = ds * x2 + s + 2 * (p1 * y + 3 * p2 * x);
+      const T b = ds * x * y + 2 * (p1 * x + p2 * y);
+      const T c = ds * y2 + s + 2 * (p2 * x + 3 * p1 * y);
+      // Invert Jacobian;
+      const T det = T(1.0) / (a * c - b * b);
+      const T alpha = det * c;
+      const T beta = -det * b;
+      const T gamma = det * a;
+      // Apply update.
+      x = x + (alpha * err_x + beta * err_y);
+      y = y + (beta * err_x + gamma * err_y);
+    }
+    return Eigen::Vector3<T>(x, y, 1);
   }
 
   CameraIntrinsicsModel GetType() const final {
@@ -136,8 +187,20 @@ absl::StatusOr<Eigen::Vector2<T>> CameraModel::ProjectPoint(
     return derived->ProjectPoint(intrinsics, point);
   }
   return absl::InvalidArgumentError(absl::StrCat(
-      "Camera model ", this->GetType(), " not supported."));
+      "ProjectPoint for camera model ", this->GetType(), " not supported."));
 }
+
+template <typename T>
+absl::StatusOr<Eigen::Vector3<T>> CameraModel::UnprojectPixel(
+    const Eigen::VectorX<T>& intrinsics,
+    const Eigen::Vector2<T>& pixel) const {
+  if (const auto derived = dynamic_cast<const OpenCv5Model*>(this)) {
+    return derived->UnprojectPixel(intrinsics, pixel);
+  }
+  return absl::InvalidArgumentError(absl::StrCat(
+      "UnprojectPixel for camera model ", this->GetType(), " not supported."));
+}
+
 } // namespace calico::sensors
 
 #endif // CALICO_SENSORS_CAMERA_MODELS_H_
---
+++ calico/sensors/camera_models_test.cpp
@@ -0,0 +1,66 @@
+#include "calico/sensors/camera_models.h"
+
+#include "Eigen/Dense"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+
+namespace calico::sensors {
+namespace {
+
+class CameraModelTest : public ::testing::Test {
+ protected:
+  static constexpr double kSamplePlaneWidth = 1.5;
+  static constexpr double kSamplePlaneHeight = 1.5;
+  static constexpr double kDelta = 0.25;
+  static constexpr int kNumXPoints = static_cast<int>(kSamplePlaneWidth / kDelta) + 1;
+  static constexpr int kNumYPoints = static_cast<int>(kSamplePlaneHeight / kDelta) + 1;
+  Eigen::Matrix3d R_world_camera;
+  Eigen::Vector3d t_world_camera;
+  std::vector<Eigen::Vector3d> t_world_points;
+
+  // Set up a synthetic world with a plane of points measuring 1.5x1.5m on the
+  // ground with a camera facing downward at 1m above the center of the plane.
+  void SetUp() override {
+    R_world_camera <<
+      1, 0, 0,
+      0, -1, 0,
+      0, 0, -1;
+    t_world_camera << kSamplePlaneWidth / 2.0, kSamplePlaneHeight / 2.0, 1.0;
+    for (int i = 0; i < kNumXPoints; ++i) {
+      for (int j = 0; j < kNumYPoints; ++j) {
+        const double x = i * kDelta;
+        const double y = j * kDelta;
+        t_world_points.push_back(Eigen::Vector3d(x, y, 0.0));
+      }
+    }
+  }
+};
+
+TEST_F(CameraModelTest, OpenCv5ModelProjectionAndUnprojection) {
+  constexpr double kSmallError = 1e-10;
+  Eigen::VectorXd intrinsics(OpenCv5Model::kNumberOfParameters);
+  intrinsics <<
+    785, 640, 400, -3.149e-1, 1.069e-1, 1.616e-4, 1.141e-4, -1.853e-2;
+  for (const Eigen::Vector3d& t_world_point : t_world_points) {
+    // Transform camera point into world coordinates.
+    const Eigen::Vector3d t_camera_point =
+      R_world_camera.transpose() * (t_world_point - t_world_camera);
+    // Forward projection.
+    absl::StatusOr<Eigen::Vector2d> pixel =
+      OpenCv5Model::ProjectPoint(intrinsics, t_camera_point);
+    ASSERT_EQ(pixel.status().code(), absl::StatusCode::kOk);
+    // Invert forward projection.
+    absl::StatusOr<Eigen::Vector3d> unprojected_point =
+      OpenCv5Model::UnprojectPixel(intrinsics, *pixel);
+    ASSERT_EQ(unprojected_point.status().code(), absl::StatusCode::kOk);
+    // Compare results.
+    const Eigen::Vector3d t_camera_point_metric =
+      t_camera_point / t_camera_point.z();
+    EXPECT_TRUE(t_camera_point_metric.isApprox(
+        *unprojected_point, kSmallError));
+  }
+}
+
+} // namespace
+} // namespace calico::sensors
