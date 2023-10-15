2af92685197b6d834140118ff9211f8f70ec2cee
James Yang <james.yang92@gmail.com>
2023-10-15 16:47:30 -0400

    Bug fixes, turned off test building in setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class CMakeBuild(build_ext):
     cfg = 'Debug' if self.debug else 'Release'
     build_args = ['--config', cfg]
 
-    cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+    cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg, '-DBUILD_TESTING=OFF']
 
     # Assuming Makefiles
     build_args += ['--']
