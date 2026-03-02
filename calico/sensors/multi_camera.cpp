0c4d05d947516bed3c264fa50dd0d4f8004aaf46
James Yang <james.yang@cosmicrobotics.com>
2026-03-02 11:19:55 -0800

    initial test fixture

---
+++ calico/sensors/multi_camera.cpp
@@ -0,0 +1,284 @@
+#include "calico/sensors/multi_camera.h"
+
+#include "calico/optimization_utils.h"
+#include "calico/sensors/camera_cost_functor.h"
+
+namespace calico::sensors {
+
+absl::Status MultiCamera::SetModel(
+    const absl::flat_hash_map<std::string, CameraIntrinsicsModel>&
+        imager_to_camera_model) {
+  for (const auto& [imager, camera_model] : imager_to_camera_model) {
+    imager_to_camera_model_[imager] = CameraModel::Create(camera_model);
+    imager_to_intrinsics_[imager] = Eigen::VectorXd::Zero(
+        imager_to_camera_model_[imager]->NumberOfParameters());
+    if (!imager_to_camera_model_[imager]) {
+      return absl::InvalidArgumentError(
+          absl::StrCat("Could not create camera model for type ", camera_model,
+                       ". It is likely not yet implemented."));
+    }
+  }
+  return absl::OkStatus();
+}
+
+absl::flat_hash_map<std::string, CameraIntrinsicsModel> MultiCamera::GetModel()
+    const {
+  absl::flat_hash_map<std::string, CameraIntrinsicsModel> imager_to_model;
+  for (const auto& [imager, model] : imager_to_camera_model_) {
+    imager_to_model[imager] = model->GetType();
+  }
+  return imager_to_model;
+}
+
+absl::Status MultiCamera::SetIntrinsics(
+    const absl::flat_hash_map<std::string, Eigen::VectorXd>&
+        imager_to_intrinsics) {
+  for (const auto& [imager, intrinsics] : imager_to_intrinsics) {
+    const auto& camera_model = imager_to_camera_model_[imager];
+    if (!camera_model) {
+      return absl::InvalidArgumentError("Camera model has not been set!");
+    }
+    if (intrinsics.size() != camera_model->NumberOfParameters()) {
+      return absl::InvalidArgumentError(absl::StrCat(
+          "Tried to set intrinsics of size ", intrinsics.size(), " for camera ",
+          GetName(), ".Expected intrinsics size of ",
+          camera_model->NumberOfParameters()));
+    }
+    imager_to_intrinsics_[imager] = intrinsics;
+  }
+  return absl::OkStatus();
+}
+
+// void Camera::EnableLatencyEstimation(bool enable) { latency_enabled_ =
+// enable; }
+
+// absl::Status Camera::SetMeasurementNoise(double sigma) {
+//   if (sigma <= 0.0) {
+//     return absl::InvalidArgumentError("Sigma must be greater than 0.");
+//   }
+//   sigma_ = sigma;
+//   return absl::OkStatus();
+// }
+
+// absl::Status Camera::UpdateResiduals(ceres::Problem& problem) {
+//   for (const auto [measurement_id, residual_id] : id_to_residual_id_) {
+//     Eigen::Vector2d residual;
+//     if (!problem.EvaluateResidualBlock(residual_id,
+//                                        /*apply_loss_function=*/false,
+//                                        nullptr, residual.data(), nullptr)) {
+//       return absl::InternalError("Failed to update residual for camera " +
+//                                  name_);
+//     }
+//     id_to_residual_[measurement_id] = residual;
+//   }
+//   return absl::OkStatus();
+// }
+
+// void Camera::ClearResidualInfo() {
+//   id_to_residual_id_.clear();
+//   id_to_residual_.clear();
+// }
+
+// void Camera::SetLossFunction(utils::LossFunctionType loss, double scale) {
+//   loss_function_ = loss;
+//   loss_scale_ = scale;
+// }
+
+// absl::StatusOr<int> Camera::AddParametersToProblem(ceres::Problem& problem) {
+//   int num_parameters_added = 0;
+//   if (!camera_model_) {
+//     return absl::FailedPreconditionError(
+//         "Cannot add camera parameters. Camera model is not yet defined.");
+//   }
+//   problem.AddParameterBlock(intrinsics_.data(), intrinsics_.size());
+//   num_parameters_added += intrinsics_.size();
+//   num_parameters_added += utils::AddPoseToProblem(problem,
+//   T_sensorrig_sensor_); problem.AddParameterBlock(&latency_, 1);
+//   ++num_parameters_added;
+//   if (!intrinsics_enabled_) {
+//     problem.SetParameterBlockConstant(intrinsics_.data());
+//   }
+//   if (!extrinsics_enabled_) {
+//     utils::SetPoseConstantInProblem(problem, T_sensorrig_sensor_);
+//   }
+//   if (!latency_enabled_) {
+//     problem.SetParameterBlockConstant(&latency_);
+//   }
+//   return num_parameters_added;
+// }
+
+// absl::StatusOr<int> Camera::AddResidualsToProblem(
+//     ceres::Problem& problem, Trajectory& sensorrig_trajectory,
+//     WorldModel& world_model) {
+//   int num_residuals_added = 0;
+//   for (const auto& [observation_id, measurement] : id_to_measurement_) {
+//     // Skip measurements marked as outliers.
+//     if (outlier_ids_.contains(observation_id)) {
+//       continue;
+//     }
+//     const int rigidbody_id = observation_id.model_id;
+//     if (!world_model.rigidbodies().contains(rigidbody_id)) {
+//       return absl::FailedPreconditionError(absl::StrCat(
+//           "Attempted to create cost function from an observation for a "
+//           "rigidbody with id ",
+//           rigidbody_id,
+//           " that does not exist in "
+//           "the world model."));
+//     }
+//     // Get the right rigidbody reference from the world model.
+//     std::unique_ptr<RigidBody>& rigidbody_ref =
+//         world_model.rigidbodies().at(rigidbody_id);
+//     Eigen::Vector3d& t_model_point =
+//         rigidbody_ref->model_definition.at(observation_id.feature_id);
+//     // Construct a cost function and supply parameters for this residual.
+//     std::vector<double*> parameters;
+
+//     ceres::CostFunction* cost_function =
+//     CameraCostFunctor::CreateCostFunction(
+//         measurement.pixel, sigma_, camera_model_->GetType(), intrinsics_,
+//         T_sensorrig_sensor_, latency_, t_model_point,
+//         rigidbody_ref->T_world_rigidbody, sensorrig_trajectory,
+//         observation_id.stamp, parameters);
+//     ceres::LossFunction* loss_function =
+//         CreateLossFunction(loss_function_, loss_scale_);
+//     const auto residual_block_id =
+//         problem.AddResidualBlock(cost_function, loss_function, parameters);
+//     id_to_residual_id_[observation_id] = residual_block_id;
+//     num_residuals_added += 1;
+//   }
+//   return num_residuals_added;
+// }
+
+// absl::StatusOr<std::vector<CameraMeasurement>> Camera::Project(
+//     const std::vector<double>& interp_times,
+//     const Trajectory& sensorrig_trajectory,
+//     const WorldModel& world_model) const {
+//   std::vector<Pose3d> poses_world_sensorrig;
+//   ASSIGN_OR_RETURN(poses_world_sensorrig,
+//                    sensorrig_trajectory.Interpolate(interp_times));
+//   std::vector<CameraMeasurement> measurements;
+//   int image_id = 0;
+//   for (int i = 0; i < interp_times.size(); ++i) {
+//     const Pose3d& T_world_sensorrig = poses_world_sensorrig.at(i);
+//     const double& stamp = interp_times.at(i);
+//     const Pose3d T_camera_world =
+//         (T_world_sensorrig * T_sensorrig_sensor_).inverse();
+//     // Project all landmarks.
+//     for (const auto& [landmark_id, landmark] : world_model.landmarks()) {
+//       const Eigen::Vector3d point_camera = T_camera_world * landmark->point;
+//       if (point_camera.z() <= 0) {
+//         continue;
+//       }
+//       const absl::StatusOr<Eigen::Vector2d> projection =
+//           camera_model_->ProjectPoint(intrinsics_, point_camera);
+//       measurements.push_back(
+//           {*projection,
+//            {stamp + latency_, image_id, kLandmarkFrameId, landmark_id}});
+//     }
+//     // Project all rigid bodies.
+//     for (const auto& [rigidbody_id, rigidbody] : world_model.rigidbodies()) {
+//       const Pose3d T_camera_rigidbody =
+//           T_camera_world * rigidbody->T_world_rigidbody;
+//       for (const auto& [point_id, point] : rigidbody->model_definition) {
+//         const Eigen::Vector3d point_camera = T_camera_rigidbody * point;
+//         if (point_camera.z() <= 0) {
+//           continue;
+//         }
+//         const absl::StatusOr<Eigen::Vector2d> projection =
+//             camera_model_->ProjectPoint(intrinsics_, point_camera);
+//         measurements.push_back(
+//             {*projection,
+//              {stamp + latency_, image_id, rigidbody_id, point_id}});
+//       }
+//     }
+//     ++image_id;
+//   }
+//   return measurements;
+// }
+
+// absl::Status Camera::AddMeasurement(const CameraMeasurement& measurement) {
+//   if (id_to_measurement_.contains(measurement.id)) {
+//     return absl::InvalidArgumentError(absl::StrCat(
+//         "Tried to add redundant measurement - Image id: ",
+//         measurement.id.image_id, ", model id: ", measurement.id.model_id,
+//         ", feature id: ", measurement.id.feature_id));
+//   }
+//   id_to_measurement_[measurement.id] = measurement;
+//   return absl::OkStatus();
+// }
+
+// absl::Status Camera::AddMeasurements(
+//     const std::vector<CameraMeasurement>& measurements) {
+//   std::string message;
+//   for (const auto& measurement : measurements) {
+//     absl::Status status = AddMeasurement(measurement);
+//     if (!status.ok()) {
+//       message += std::string(status.message()) + "\n";
+//     }
+//   }
+//   if (message.empty()) {
+//     return absl::OkStatus();
+//   }
+//   return absl::InvalidArgumentError(message);
+// }
+
+// const absl::flat_hash_map<CameraObservationId, CameraMeasurement>&
+// Camera::GetMeasurementIdToMeasurement() const {
+//   return id_to_measurement_;
+// }
+
+// absl::StatusOr<std::vector<std::pair<CameraMeasurement, Eigen::Vector2d>>>
+// Camera::GetMeasurementResidualPairs() const {
+//   if (id_to_residual_.size() > id_to_measurement_.size()) {
+//     return absl::InternalError("There are more residuals than
+//     measurements.");
+//   }
+//   if (id_to_measurement_.empty()) {
+//     return absl::FailedPreconditionError(
+//         "Measurements are empty. Nothing to return.");
+//   }
+//   std::vector<std::pair<CameraMeasurement, Eigen::Vector2d>> pairs;
+//   for (const auto [id, residual] : id_to_residual_) {
+//     auto it = id_to_measurement_.find(id);
+//     if (it != id_to_measurement_.end()) {
+//       pairs.push_back({it->second, residual});
+//     } else {
+//       return absl::InternalError(
+//           "Found a residual that doesn't correspond to any measurement.");
+//     }
+//   }
+//   return pairs;
+// }
+
+// absl::Status Camera::MarkOutlierById(const CameraObservationId& id) {
+//   if (!id_to_measurement_.contains(id)) {
+//     return absl::InvalidArgumentError(absl::StrCat(
+//         "Attempted to add id that is not within the measurement set.",
+//         "\nStamp - ", id.stamp, ", image_id - ", id.image_id, "model_id - ",
+//         id.model_id, "feature_id - ", id.feature_id));
+//   }
+//   outlier_ids_.insert(id);
+//   return absl::OkStatus();
+// }
+
+// absl::Status Camera::MarkOutliersById(
+//     const std::vector<CameraObservationId>& ids) {
+//   for (const auto& id : ids) {
+//     RETURN_IF_ERROR(MarkOutlierById(id));
+//   }
+//   return absl::OkStatus();
+// }
+
+// void Camera::ClearOutliersList() { outlier_ids_.clear(); }
+
+// void Camera::ClearMeasurements() {
+//   id_to_measurement_.clear();
+//   id_to_residual_id_.clear();
+//   id_to_residual_.clear();
+//   outlier_ids_.clear();
+// }
+
+// int Camera::NumberOfMeasurements() const { return id_to_measurement_.size();
+// }
+
+}  // namespace calico::sensors
---
+++ calico/sensors/multi_camera.h
@@ -0,0 +1,236 @@
+#ifndef CALICO_SENSORS_MULTI_CAMERA_H_
+#define CALICO_SENSORS_MULTI_CAMERA_H_
+
+#include <string>
+#include <vector>
+
+#include "Eigen/Dense"
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/status/status.h"
+#include "absl/strings/string_view.h"
+#include "calico/sensors/camera.h"
+#include "calico/sensors/camera_models.h"
+#include "calico/sensors/sensor_base.h"
+#include "calico/trajectory.h"
+#include "calico/typedefs.h"
+#include "ceres/problem.h"
+
+namespace calico::sensors {
+
+/// Multi-camera class. This class can be used to calibrate multiple imagers on
+/// a single module, for example, stereo cameras.
+class MultiCamera : public Sensor {
+ public:
+  explicit MultiCamera() = default;
+  MultiCamera(const MultiCamera&) = delete;
+  MultiCamera& operator=(const MultiCamera&) = delete;
+  ~MultiCamera() = default;
+
+  // /// Top-level sensor module name.
+  void SetName(const std::string& name) { name_ = name; }
+  const std::string& GetName() const { return name_; }
+
+  /// Setter for the camera model.
+  absl::Status SetModel(
+      const absl::flat_hash_map<std::string, CameraIntrinsicsModel>&
+          camera_model);
+
+  /// Getter for the camera model.
+  absl::flat_hash_map<std::string, CameraIntrinsicsModel> GetModel() const;
+
+  /// Sets extrinsics for the camera module itself. Each sensor will internally
+  /// have an additional extrinsics offset relative to this one. This can be set
+  /// to a constant CAD value.
+  void SetSensorExtrinsics(const Pose3d& pose_sensorrig_from_sensor) {
+    pose_sensorrig_from_sensor_ = pose_sensorrig_from_sensor;
+  }
+  const Pose3d& GetSensorExtrinsics() const {
+    return pose_sensorrig_from_sensor_;
+  }
+
+  /// Set the extrinsics for each internal sensor. This transform will be
+  /// applied on top of pose_sensorrig_from_sensor which is held constant during
+  /// optimization.
+  void SetImagerExtrinsics(const absl::flat_hash_map<std::string, Pose3d>&
+                               imager_to_pose_sensor_from_imager) {
+    imager_to_pose_sensor_from_imager_ = imager_to_pose_sensor_from_imager;
+  }
+  const absl::flat_hash_map<std::string, Pose3d>& GetImagerExtrinsics() const {
+    return imager_to_pose_sensor_from_imager_;
+  }
+
+  absl::Status SetIntrinsics(
+      const absl::flat_hash_map<std::string, Eigen::VectorXd>& intrinsics);
+  const absl::flat_hash_map<std::string, Eigen::VectorXd>& GetIntrinsics()
+      const {
+    return imager_to_intrinsics_;
+  }
+  // absl::Status SetLatency(double latency) {
+  //   latency_ = latency;
+  //   return absl::OkStatus();
+  // }
+  // double GetLatency() const { return latency_; }
+  // void EnableExtrinsicsEstimation(
+  //     const absl::flat_hash_map<std::string, bool> imager_to_enable) {}
+  // void EnableIntrinsicsEstimation(
+  //     const absl::flat_hash_map<std::string, bool>& enable) {
+  //   extrinsics_enabled_ = false;
+  // }
+  // void EnableLatencyEstimation(bool enable);
+  void SetLossFunction(utils::LossFunctionType loss, double scale) final {
+    loss_function_ = loss;
+    loss_scale_ = scale;
+  }
+  absl::StatusOr<int> AddParametersToProblem(ceres::Problem& problem) final {
+    return absl::UnimplementedError(
+        "AddParametersToProblem not implemented for MultiCamera.");
+  }
+  absl::StatusOr<int> AddResidualsToProblem(ceres::Problem& problem,
+                                            Trajectory& sensorrig_trajectory,
+                                            WorldModel& world_model) final {
+    return absl::UnimplementedError(
+        "AddResidualsToProblem not implemented for MultiCamera.");
+  }
+  absl::Status SetMeasurementNoise(double sigma) final {
+    if (sigma > 0) {
+      sigma_ = sigma;
+      return absl::OkStatus();
+    }
+    return absl::InvalidArgumentError(
+        absl::StrCat("Cannot set ", GetName(), " measurement noise to ", sigma,
+                     ". Measurement noise must be positive."));
+  }
+  absl::Status UpdateResiduals(ceres::Problem& problem) final {
+    return absl::UnimplementedError(
+        "UpdateResiduals not implemented for MultiCamera.");
+  }
+  void ClearResidualInfo() final {
+    for (auto& [_, id_to_residual_] : imager_to_id_to_residual_) {
+      id_to_residual_.clear();
+    }
+    for (auto& [_, id_to_residual_id_] : imager_to_id_to_residual_id_) {
+      id_to_residual_id_.clear();
+    }
+  }
+
+  // /// Compute synthetic camera measurements given a Trajectory and
+  // WorldModel.
+
+  // /// This method projects the world model through the kinematic chain at
+  // given
+  // /// timestamps. This method returns only valid synthetic measurements as
+  // would
+  // /// be observed by the actual sensor, complying with physicality such as
+  // /// features being in front of the camera. Returns measurements in the
+  // order
+  // /// of the interpolation timestamps.\n\n
+  // /// `interp_times` is a vector of timestamps in seconds at which
+  // /// `sensorrig_trajectory` will be interpolated. No assumptions are made
+  // about
+  // /// timestamp uniqueness or order.\n\n
+  // /// `sensorrig_trajectory` is the world-from-sensorrig trajectory
+  // /// \f$\mathbf{T}^w_r(t)\f$.\n\n
+  // absl::StatusOr<
+  //     absl::flat_hash_map<std::string, std::vector<CameraMeasurement>>>
+  // Project(const std::vector<double>& interp_times,
+  //         const Trajectory& sensorrig_trajectory,
+  //         const WorldModel& world_model) const;
+
+  // /// Setter for the camera models.
+  // absl::Status SetMultiCameraModel(
+  //     const absl::flat_hash_map<std::string, CameraIntrinsicsModel>&
+  //         imager_name_to_camera_model);
+
+  // /// Getter for the camera model.
+  // absl::flat_hash_map<std::string, CameraIntrinsicsModel>&
+  // GetMultiCameraModel()
+  //     const;
+
+  // /// Add a single camera measurement to the measurement list.
+
+  // /// Returns an error if the measurement's id is duplicated without adding.
+  // absl::Status AddMeasurement(const CameraMeasurement& measurement);
+
+  // /// Add multiple measurements to the measurement list.
+
+  // /// Returns an error status if any measurements are duplicates within its
+  // /// internally managed set of measurements.\n\n
+  // /// **Note: If this method encounters any duplicates, it will STILL attempt
+  // to
+  // /// add the entire vector. If it returns an error status, it means that all
+  // /// unique measurements have been added, but duplicates have been
+  // skipped.** absl::Status AddMeasurements(
+  //     const std::vector<CameraMeasurement>& measurements);
+
+  // /// Getter for all measurements. Returns a map of observation ids to
+  // /// measurements. Will be empty if there are no measurements.
+  // const absl::flat_hash_map<CameraObservationId, CameraMeasurement>&
+  // GetMeasurementIdToMeasurement() const;
+
+  // /// Returns a vector of measurement-residual pairs.
+
+  // /// Only returns for measurements that have residuals. Returns an error if
+  // /// there are more residuals than measurements, or if there are no
+  // /// measurements.\n\n
+  // /// **Note: This method will only return residuals for measurements that
+  // have
+  // /// NOT been marked as outliers.**
+  // absl::StatusOr<std::vector<std::pair<CameraMeasurement, Eigen::Vector2d>>>
+  // GetMeasurementResidualPairs() const;
+
+  // /// Tag a single measurement as an outlier by its measurement ID.
+
+  // /// Camera class keeps track of an outliers list internally. If passed a
+  // /// measurement ID that does not correspond with any measurement tracked by
+  // /// this camera, an InvalidArgument status is returned.
+  // absl::Status MarkOutlierById(const CameraObservationId& id);
+
+  // /// Tag multiple measurements as outliers by measurement ID.
+
+  // /// Camera class keeps track of an outliers list internally. If passed a
+  // /// measurement ID that does not correspond with any measurement tracked by
+  // /// this camera, an InvalidArgument status is returned.
+  // absl::Status MarkOutliersById(const std::vector<CameraObservationId>& ids);
+
+  // /// Clear outliers list.
+  // void ClearOutliersList();
+
+  // /// Clear all measurements.
+
+  // /// This will also clear any internally stored residuals and marked
+  // outliers. void ClearMeasurements();
+
+  // /// Get current number of measurements stored.
+  // int NumberOfMeasurements() const;
+
+ private:
+  std::string name_;
+  Pose3d pose_sensorrig_from_sensor_;
+  absl::flat_hash_map<std::string, bool> intrinsics_enabled_;
+  absl::flat_hash_map<std::string, bool> extrinsics_enabled_;
+  absl::flat_hash_map<std::string, bool> latency_enabled_;
+  absl::flat_hash_map<std::string, std::unique_ptr<CameraModel>>
+      imager_to_camera_model_;
+  absl::flat_hash_map<std::string, Pose3d> imager_to_pose_sensor_from_imager_;
+  absl::flat_hash_map<std::string, Eigen::VectorXd> imager_to_intrinsics_;
+  absl::flat_hash_map<std::string, double> imager_to_latency_;
+  double sigma_ = 1.0;
+  utils::LossFunctionType loss_function_;
+  double loss_scale_ = 1.0;
+  absl::flat_hash_map<
+      std::string, absl::flat_hash_map<CameraObservationId, CameraMeasurement>>
+      imager_to_id_to_measurement_;
+  absl::flat_hash_map<std::string,
+                      absl::flat_hash_map<CameraObservationId, Eigen::Vector2d>>
+      imager_to_id_to_residual_;
+  absl::flat_hash_map<std::string, absl::flat_hash_map<CameraObservationId,
+                                                       ceres::ResidualBlockId>>
+      imager_to_id_to_residual_id_;
+  absl::flat_hash_map<std::string, absl::flat_hash_set<CameraObservationId>>
+      imager_to_outlier_ids_;
+};
+
+}  // namespace calico::sensors
+
+#endif  // CALICO_SENSORS_CAMERA_H_
